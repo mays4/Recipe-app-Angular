691043b21a9f205179d68097bc9566af10d604b0
mays4 <maysalani84@yahoo.com>
2022-07-20 16:10:11 -0400

    add components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[]=[
+    new Recipe('A Test REcipe','This si a simpel recipe','https://www.averiecooks.com/wp-content/uploads/2021/01/garlicbutterchicken-5.jpg')
+  ];
 
   constructor() { }
 

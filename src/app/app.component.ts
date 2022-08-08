4824be16d2d095f597e2923d3a8b1931998d0c49
mays4 <maysalani84@yahoo.com>
2022-08-08 16:27:12 -0400

    adding style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as AuthActions from './auth/store/auth.actions'
 import { AuthService } from './auth/auth.service';
 import { LoggingService } from './logging.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,18 @@ import { LoggingService } from './logging.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService:AuthService,private loggingService:LoggingService,private store:Store){}
+  constructor(private authService:AuthService,private loggingService:LoggingService,private store:Store,
+  @Inject(PLATFORM_ID) private platformId:any
+    ){}
   // loadedFeatures = 'shopping-list'
   // onNavigate(feature:string){
   //   this.loadedFeatures = feature;
   // }
   ngOnInit(){
-   this.store.dispatch(new AuthActions.AutoLogin() );
-  //  this.loggingService.printLog('hello from app comp')
+    if(isPlatformBrowser(this.platformId)){
+      this.store.dispatch(new AuthActions.AutoLogin() );
+    }
+  //  this.store.dispatch(new AuthActions.AutoLogin() );
+   this.loggingService.printLog('hello from app comp')
   }
 }
